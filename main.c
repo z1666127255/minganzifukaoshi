e52d539491d0710356cf865a71e954867469f99b
曾英峰 <1666127255@qq.com>
2019-01-17 09:21:29 +0800

    输入一段文字

--- main.c
+++ main.c
@@ -2,6 +2,23 @@
 
 char arr[1024];
  int index = 0;
+ int isMingan(char x)
+ {
+     int count = 0;
+
+     for(int i = 0;i<index;i++)
+     {
+         if(x=arr[i]){
+             count++;
+         }
+     }
+     if(count = 0){
+         return 0;
+     }
+     else{
+         return 1;
+     }
+ }
 int main()
 {
 
@@ -56,7 +73,23 @@ int main()
         }
         if (code == 4)
         {
-
+            printf("qingshuruyiduanwenzi\n");
+            char neirong[1024];
+            scanf("%s",neirong);
+            for(int i=0;1;i++){
+                if(neirong[i]=='\0'){
+                    break;
+                }
+                if(isMingan(arr[i])==1){
+                    neirong[i]=='*';
+                }
+                printf("\n");
+                printf("dianjihuichejixxu\n");
+                char x;
+                scanf("%c",&x);
+                scanf("%c",&x);
+                
+            }
         }
         if (code == 5)
         {
