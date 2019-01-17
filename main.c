e03026be6ac231e7821dc57acc5e3bf124845b1a
曾英峰 <1666127255@qq.com>
2019-01-17 08:45:20 +0800

    基本框架

---
+++ main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+char arr[1024];
+ int index = 0;
+int main()
+{
+
+    printf("minganzifuguanlixitong\n");
+
+    printf("1----tianjia\n");
+    printf("2----shanchu\n");
+    printf("3----xianshisuoyou\n");
+    printf("4----dayiduanwenzi\n");
+    printf("5----tuichu\n");
+
+    printf("xuanzeguanlixitong\n");
+
+    int code;
+    scanf("%c", &code);
+    scanf("%c", &code);
+
+        if (code == 1)
+        {
+
+        }
+        if (code == 2)
+        {
+
+        }
+        if (code == 3)
+        {
+
+        }
+        if (code == 4)
+        {
+
+        }
+        if (code == 5)
+        {
+
+        }
+
+    return 0;
+}
